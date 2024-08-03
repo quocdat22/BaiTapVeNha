0d932fa6ab61f450eedacef295d96d035a876a75
Hoang Le Quoc Dat <hoanglequocdat132002@gmail.com>
2024-08-03 18:30:51 +0700

    buoi1_bai9

---
+++ BTVN_Dat_Phong/BTVN_Dat_Phong/BaiTapVeNha.h
@@ -0,0 +1,12 @@
+#ifndef BAITAP_H
+#define BAITAP_H
+
+//buoi1_bai9
+void buoi1_bai9();
+
+
+
+
+
+
+#endif
---
+++ BTVN_Dat_Phong/BTVN_Dat_Phong/main.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "BaiTapVeNha.h"
+
+void main()
+{
+	//buoi1_bai11();
+	//buoi1_bai10();
+	buoi1_bai9();
+}
